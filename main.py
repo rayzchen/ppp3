144f0a8e444c6d62cf7de2d059f44d9931e438a8
Ray Chen <tankimarshal2@gmail.com>
2025-02-10 22:30:25 +0000

    Display quesiton and answer images

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
 from PySide6.QtCore import *
 from PySide6.QtWidgets import *
 from PySide6.QtGui import *
+import random
+import functools
+import os
 import sys
+
+questions = {}
+answers = {}
+topics = [
+    "algorithms",
+    # "programming",
+    # "data representation",
+    # "computer hardware",
+    # "networks",
+    # "cyber security",
+    # "ethics"
+]
+
+def load_questions():
+    for topic in topics:
+        questions[topic] = []
+        answers[topic] = []
+        files = os.listdir(os.path.join("questions", topic))
+        question_nums = [file[:-5] for file in files if file.endswith("q.png")]
+        for number in question_nums:
+            path = os.path.join("questions", topic, number + "q.png")
+            questions[topic].append(QPixmap(path))
+            path = os.path.join("questions", topic, number + "a.png")
+            answers[topic].append(QPixmap(path))
 
 class QuizWindow(QMainWindow):
     def __init__(self, parent=None):
@@ -31,6 +58,15 @@ class QuizWindow(QMainWindow):
         self.centralWidget().setParent(None)
         self.question_view.reset()
         self.setCentralWidget(self.question_view)
+
+    def show_random_question(self):
+        topic = random.choice(topics)
+        self.show_topic_question(topic)
+
+    def show_topic_question(self, topic):
+        self.question_view.topic = topic
+        self.question_view.question = random.randrange(len(questions[topic]))
+        self.switch_question_view()
 
 class TitleView(QWidget):
     def __init__(self, parent=None):
@@ -68,7 +104,7 @@ class TitleView(QWidget):
 
     def register_buttons(self, window):
         self.topic_button.clicked.connect(window.switch_topic_view)
-        self.question_button.clicked.connect(window.switch_question_view)
+        self.question_button.clicked.connect(window.show_random_question)
         self.quit_button.clicked.connect(window.close)
 
 class TopicView(QWidget):
@@ -83,15 +119,6 @@ class TopicView(QWidget):
         self.button_layout.setSpacing(0)
 
         self.topic_buttons = []
-        topics = [
-            "algorithms",
-            "programming",
-            "data representation",
-            "computer hardware",
-            "networks",
-            "cyber security",
-            "ethics"
-        ]
         for topic in topics:
             button = QPushButton(topic, self)
             self.button_layout.addWidget(button)
@@ -110,7 +137,10 @@ class TopicView(QWidget):
     def register_buttons(self, window):
         self.top_bar.register_buttons(window)
         for button in self.topic_buttons:
-            button.clicked.connect(window.switch_question_view)
+            button.clicked.connect(functools.partial(
+                window.show_topic_question,
+                button.text()
+            ))
 
 class TopBar(QWidget):
     def __init__(self, parent=None):
@@ -122,6 +152,7 @@ class TopBar(QWidget):
         self.hbox.addWidget(self.menu_button, 2)
         self.hbox.addStretch(1)
         self.title = QLabel("GCSE COMPUTER SCIENCE\nQUESTION OF THE DAY", self)
+        self.title.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.hbox.addWidget(self.title)
         self.hbox.addStretch(1)
         self.quit_button = QPushButton("QUIT")
@@ -144,26 +175,31 @@ class QuestionView(QWidget):
 
         self.question_layout = QHBoxLayout(self.question_container)
         self.question_widget = QLabel("filler text", self)
+        self.question_widget.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.question_widget.setSizePolicy(
-            QSizePolicy.Policy.Preferred,
-            QSizePolicy.Policy.Preferred
+            QSizePolicy.Policy.Ignored,
+            QSizePolicy.Policy.Ignored
         )
         self.question_layout.addWidget(self.question_widget, 1)
 
         self.answer_button = QPushButton("CLICK TO SHOW\nMARK SCHEME")
         self.answer_button.setFont(QFont("Bebas Neue", 48))
         self.answer_button.setSizePolicy(
-            QSizePolicy.Policy.Preferred,
-            QSizePolicy.Policy.Preferred
+            QSizePolicy.Policy.Ignored,
+            QSizePolicy.Policy.Ignored
         )
         self.answer_button.clicked.connect(self.show_mark_scheme)
         self.question_layout.addWidget(self.answer_button, 1)
 
         self.mark_scheme_widget = QLabel("mark scheme")
+        self.mark_scheme_widget.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.mark_scheme_widget.setSizePolicy(
-            QSizePolicy.Policy.Preferred,
-            QSizePolicy.Policy.Preferred
+            QSizePolicy.Policy.Ignored,
+            QSizePolicy.Policy.Ignored
         )
+
+        self.topic = "algorithms"
+        self.question = 0
 
     def show_mark_scheme(self):
         widget = self.question_layout.itemAt(1).widget()
@@ -177,11 +213,31 @@ class QuestionView(QWidget):
         widget.setParent(None)
         self.question_layout.addWidget(self.answer_button, 1)
 
+    def paintEvent(self, event):
+        super().paintEvent(event)
+        self.resize_images()
+
+    def resize_images(self):
+        scaled = questions[self.topic][self.question].scaled(
+            self.question_widget.size(),
+            Qt.AspectRatioMode.KeepAspectRatio,
+            Qt.TransformationMode.SmoothTransformation
+        )
+        self.question_widget.setPixmap(scaled)
+
+        scaled = answers[self.topic][self.question].scaled(
+            self.mark_scheme_widget.size(),
+            Qt.AspectRatioMode.KeepAspectRatio,
+            Qt.TransformationMode.SmoothTransformation
+        )
+        self.mark_scheme_widget.setPixmap(scaled)
+
     def register_buttons(self, window):
         self.top_bar.register_buttons(window)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    load_questions()
     window = QuizWindow()
     window.show()
     sys.exit(app.exec())
